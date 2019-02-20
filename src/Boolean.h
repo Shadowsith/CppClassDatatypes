c7cba33a3decfd8843088d86365f3c8de6907542
Shadowsith <shadowsith94@gmail.com>
2019-02-20 20:41:29 +0100

    some changes to header-only lib

--- src/Boolean.h
+++ src/Boolean.h
@@ -8,23 +8,65 @@ namespace dt {
     class Boolean : Datatype {
 
         private:
-            bool _internal = false;
+        bool _internal = false;
 
         public:
-            Boolean();
-            Boolean(int value);
-            Boolean(const char* value);
-            Boolean(std::string value);
+        Boolean() {}
 
-            bool getValue();
-            bool compareTo(Boolean b);
-            bool compareTo(bool b);
-            std::string toString();
-            static std::string toString(Boolean b);
-            static std::string toString(bool b);
-            static Boolean valueOf(bool b);
-            static Boolean valueOf(const char* cs);
-            static Boolean vlaueOf(std::string s);
+        Boolean(int i) {
+            if(i == 0) _internal = false;
+            else _internal = true;
+        }
+
+        Boolean(const char* s) {
+            if(s == "true") _internal = true;
+            else _internal = false;
+        }
+
+        Boolean(std::string s) {
+            if(s == "true") _internal = true;
+            else _internal = false;
+        }
+
+        bool getValue() { return _internal }
+
+        bool compareTo(Boolean &b) {
+            return (this->getValue() == b.getValue())
+        }
+
+        bool compareTo(bool b) {
+            return (this->getValue() == b);
+        }
+
+        std::string toString() {
+            if(_internal) return "true";
+            else return "false";
+        }
+
+        static std::string toString(Boolean b) {
+            if(b.getValue()) return "true";
+            else return "false";
+        }
+
+        static std::string toString(bool b) {
+            if(b) return "true";
+            else return "false";
+        }
+
+        static Boolean valueOf(bool b) {
+            Boolean bo(b);
+            return bo;
+        }
+
+        static Boolean valueOf(const char* cs) {
+            Boolean bo(cs);
+            return bo;
+        }
+
+        static Boolean vlaueOf(std::string s) {
+            Boolean bo(s);
+            return bo;
+        }
     };
 }
 
