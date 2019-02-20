37d8ae99ae43cac6e205dd2a869337cb4ce97a4a
Shadowsith <shadowsith94@gmail.com>
2019-02-20 23:09:07 +0100

    update

--- src/Float.h
+++ src/Float.h
@@ -1,9 +1,62 @@
 #ifndef FLOAT_H
 #define FLOAT_H
 
-class Float {
+#include "Datatype.h"
+#include <string>
+#include <cstdlib>
 
-i}
+class Float : Datatype {
+    private:
+    float _internal = 0.0;
+
+    public:
+    Float() {}
+    Float(float f) { _internal = f }
+
+    short shortValue() {
+        return static_cast<short>(_internal);
+    }
+
+    int intValue() {
+        return static_cast<int>(_internal);
+    }
+
+    long longValue() {
+        return static_cast<long>(_internal);
+    }
+
+    double doubleValue() {
+        return static_cast<double>(_internal);
+    }
+
+    static float parse(short s) {
+        return static_cast<float>(s);
+    }
+
+    static float parse(int i) {
+        return static_cast<float>(i);
+    }
+
+    static float parse(long l) {
+        return static_cast<float>(l);
+    }
+
+    static float parse(double d) {
+        return static_cast<float>(d);
+    }
+
+    static float parse(const char* s) {
+        return std::atof(s);
+    }
+
+    static float parse(std::string s) {
+        return std::stof(s);
+    }
+
+    std::string toString() {
+        return std::to_string(_internal);
+    }
+}
 
 #endif //FLOAT_H 
 
